77fe288bc64dba098cb042efc60bee282cdb7d79
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:48 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/view/all/AllView.java
+++ src/main/java/com/semanticcms/view/all/AllView.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-view-all - SemanticCMS view of all content of the current page and all child pages.
- * Copyright (C) 2016, 2017, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-view-all.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.view.all;
 
 import com.aoapps.html.servlet.FlowContent;

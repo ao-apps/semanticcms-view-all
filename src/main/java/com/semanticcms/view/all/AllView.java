aa3d895623313cafb11ebf13920540beac1f4077
AO Industries, Inc <support@aoindustries.com>
2026-01-29 01:31:55 -0600

    Workaround javadoc reproducibility limitations/bugs
    Replaced unqualified javadocs links to include class name to help reproducible
    builds.
    
    Java 21 (and probably other versions) have been inconsistent with their HTML
    generation when using references to something without the classname, such as
    {@link #close()}.
    
    This commit is a wide-scale scripted addition of classname before the '#'. A
    few manual changes are made where the automated replacement failed, such as
    unqualified docs related to a class that appear in the class docs (before the
    class line).
    
    Following this commit, we will also be adding Git commit hooks and Jenkins
    builds steps to enforce this constraint.

--- src/main/java/com/semanticcms/view/all/AllView.java
+++ src/main/java/com/semanticcms/view/all/AllView.java
@@ -66,7 +66,7 @@ public final class AllView extends View {
   private static final String JSPX_TARGET = "/semanticcms-view-all/view.inc.jspx";
 
   /**
-   * Registers the "{@link #NAME}" view in {@link RegistryEE} and {@link SemanticCMS}.
+   * Registers the "{@link AllView#NAME}" view in {@link RegistryEE} and {@link SemanticCMS}.
    */
   @WebListener("Registers the \"" + NAME + "\" view in RegistryEE and SemanticCMS.")
   public static class Initializer implements ServletContextListener {
